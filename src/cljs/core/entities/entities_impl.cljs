361605f1ca786fc56365e2ea35e5029cfa04971e
voytech <wojmak@gmail.com>
2015-05-06 21:46:29 +0200

    added initial text creation and edition popups

--- src/cljs/core/entities/entities_impl.cljs
+++ src/cljs/core/entities/entities_impl.cljs
@@ -5,25 +5,31 @@
             [utils.popups :as pp]
             [utils.canvas-events-tests :as cet]))
 
-(defn- show-at-func [event-wrapper popup-id]
-  (let [event (.-e event-wrapper)
-        crd   (cet/client-coords event)]
-    (when (= (.-which event) 3)
-      (println (pp/get-popup popup-id))
-      (p/show-at (pp/get-popup popup-id) (:x crd) (:y crd)))))
+(defn- show-at-func
+  ([event-wrapper popup-id bttn]
+     (let [event (.-e event-wrapper)
+           crd   (cet/client-coords event)]
+       (when (= (.-which event) bttn)
+         (println (pp/get-popup popup-id))
+         (p/show-at (pp/get-popup popup-id) (:x crd) (:y crd)))))
+  ([src popup-id]
+     (let [crd {:x (.-left src) :y (.-top src)}]
+         (println (pp/get-popup popup-id))
+         (p/show-at (pp/get-popup popup-id) (:x crd) (:y crd)))))
 
 (defn create-image-entity [data params]
   (let [img (js/fabric.Image.
                             data
                             (clj->js params))]
     (entities/create-entity "image" img
-                            {"mouseup" #(show-at-func % "editing")})
+                            {"mouseup" #(show-at-func % "editing" 3)})
                             ))
 
 (defn create-text-entity [text params]
   (let [texte (js/fabric.Text. text (clj->js params))]
     (entities/create-entity "text" texte
-                            {"mouseup" #(show-at-func % "editing")})))
+                            {"mouseup" #(show-at-func % "text-edit" 3)
+                             "added"  #(show-at-func texte "text-create")})))
 
 (defn create-slot-entity [params]
   (let [slot (js/fabric.Rect. (clj->js params))]
--- src/cljs/utils/canvas_popups.cljs
+++ src/cljs/utils/canvas_popups.cljs
@@ -1,10 +1,19 @@
 (ns utils.canvas-popups
   (:require [ui.components.popup :as p]
             [utils.popups :as popups]
-            [ui.views.editing :as e]))
+            [ui.views.editing :as e]
+            [ui.views.text-popups :as tp]))
 
 (def ^:private POPUPS-HOLDER "popups-holder")
 
 (popups/make-popup "editing"
                    POPUPS-HOLDER
                    (e/editing))
+
+(popups/make-popup "text-create"
+                   POPUPS-HOLDER
+                   (tp/text-add-popup))
+
+(popups/make-popup "text-edit"
+                   POPUPS-HOLDER
+                   (tp/text-edit-popup))

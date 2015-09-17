6ae754df2eae77131f6df9a49cd6a43690808d43
voytech <wojmak@gmail.com>
2015-09-17 14:56:45 +0200

    Entity level optional schema step 18. [#102329304]

--- src/clj/core/db/entities.clj
+++ src/clj/core/db/entities.clj
@@ -17,9 +17,9 @@
 ; NEW APPROACH (I THINK MUCH BETTER):
 ; (defentity user.info  ;this is a prefix for attribute to define entity
 ;   (property name :username   type :db.type/string  with {:required true, unique true})
-;   (property name :roles      type :db.type/ref    cardinality :db.cardinality/many  with {:required true})
-;   (property name :tenant     type :db.type/ref    cardinality :db.cardinality/one   with {:lookup-ref #([:user.info/name %])})
-;   (property name :messages   type :db.type/ref    cardinality :db.cardinality/many  with {:related-type 'message}
+;   (property name :roles      type :db.type/ref    cardinality :db.cardinality/many  mapping-opts {:required true})
+;   (property name :tenant     type :db.type/ref    cardinality :db.cardinality/one   mapping-opts {:lookup-ref #([:user.info/name %])})
+;   (property name :messages   type :db.type/ref    cardinality :db.cardinality/many  mapping-opts {:r:elated-type 'message}
 ; Approach above will allow to:
 ; 1. Perform mapping from service entities into database entities.
 ; 2. Perform mapping from database entity into service entities.
@@ -44,8 +44,6 @@
 (def ^:dynamic mapping-opts {})
 (def ^:dynamic entities-frequencies {})
 (def ^:dynamic schema [])
-(def ^:dynamic flag false)
-(def ^:dynamic curr-entity-name "")
 
 (defn make-temp-id []
   (let [partition (or (:db-partition mapping-opts)
@@ -79,6 +77,19 @@
 (defn inject-def [ns var-name value]
  (intern ns var-name value))
 
+(defn make-var [symbol val]
+  (intern 'core.db.entities symbol val))
+
+(defn del-var [symb]
+  (ns-unmap 'core.db.entities symb))
+
+(defn get-var [symb]
+  (->> symb
+       name
+       (str "core.db.entities/")
+       symbol
+       resolve
+       var-get))
 
 (defn get-frequencies []
    (var-by-symbol 'core.db.entities/entities-frequencies))
@@ -91,17 +102,13 @@
        :db.install/_attribute :db.part/db}
       (merge (if-let [uniq  (:unique property-def)] {:db/unique uniq} {}))
       (merge (if-let [cmpnt (:component? property-def)] {:db/isComponent cmpnt} {}))
-      (merge (if-let [card  (:cardinality property-def)] {:db/cardinality card} {:db/cardinality :db.cardinality/one})))
-  )
+      (merge (if-let [card  (:cardinality property-def)] {:db/cardinality card} {:db/cardinality :db.cardinality/one}))))
 
 (defn- append-schema [next-db-property]
   (def schema (conj schema next-db-property)))
 
 (defn persist-schema []
-  (println "printing schema")
-  (println schema)
-  (d/transact (connect) schema)
-  )
+  (d/transact (connect) schema))
 
 (defn- do-check [key val]
   (when-not (and (symbol? key)
@@ -117,41 +124,42 @@
 (defmacro property [& args]
   (let [decoded (decode-args args)
         prop-name (:name decoded)
-        entity-name (name curr-entity-name)
+        entity-name (name (get-var 'curr-entity-name))
         to-property (keyword (str entity-name "/" (name (or (:to-property decoded) prop-name))))
         property-def (assoc decoded :to-property to-property)]
-    (if (not flag) (do (append-schema (create-db-property property-def))
-                       {(:name decoded)
-                        (dissoc (assoc decoded :to-property to-property) :name)})
-                   {to-property {:to-property prop-name}
+    (if (not (get-var 'flag))
+      (do (append-schema (create-db-property property-def))
+          {(:name decoded)
+           (dissoc (assoc decoded :to-property to-property) :name)})
+      {to-property {:to-property prop-name}
                     ;(dissoc (assoc decoded :to-property prop-name) :name)
-                    })))
+      })))
 
 (defn concat-into [& items]
   (into [] (apply concat items)))
 
-;TODO: create mapping-type attribute for datomic. When saving entity pass this mapping-type so that unmapping can be done.
 (defmacro defentity [entity-name & rules]
-   (d/transact (connect) (mapping-enum  (eval entity-name)))
-   (def curr-entity-name (eval entity-name))
-   (do
-      (let [entity-var (var-get (intern 'core.db.entities (eval entity-name)
-                                           {:type (eval entity-name)
-                                            :mapping (binding [flag false]
-                                                       (apply merge (mapv #(eval %) rules)))
-                                            :rev-mapping (binding [flag true]
-                                                           (apply merge (mapv #(eval %) rules)))
-                                           }
-                                           ))]
-         (when (:mapping-inference mapping-opts)
-           (let [prop-map (apply merge (mapv (fn [k] {k [(:type entity-var)]}) (-> entity-var :mapping (keys))))]
-             (def entities-frequencies (merge-with concat-into entities-frequencies prop-map))))
-         )))
+  (d/transact (connect) (mapping-enum  (eval entity-name)))
+  (make-var 'curr-entity-name (eval entity-name))
+  (let [entity-var (var-get (intern 'core.db.entities (eval entity-name)
+                                      {:type (eval entity-name)
+                                       :mapping (do (make-var 'flag false)
+                                                    (apply merge (mapv #(eval %) rules)))
+                                       :rev-mapping (do (make-var 'flag true)
+                                                        (apply merge (mapv #(eval %) rules)))
+                                       }
+                                      ))]
+      (when (:mapping-inference mapping-opts)
+        (let [prop-map (apply merge (mapv (fn [k] {k [(:type entity-var)]}) (-> entity-var :mapping (keys))))]
+          (def entities-frequencies (merge-with concat-into entities-frequencies prop-map)))))
+  (del-var 'curr-entity-name)
+  (del-var 'flag)
+  identity
+  )
 
 (defmacro defschema [opts & defentities]
-  (def ^:dynamic flag false)
   (let [options (eval opts)]
-    (def ^:dynamic mapping-opts options))
+    (def ^:dynamic mapping-opts options)) ;do alter-var-root
   (initialize-database)
   (d/transact (connect) [ENTITY_TYPE_ATTRIB])
   (eval defentities)
@@ -193,11 +201,11 @@
 (defmulti apply-mapping-opts :opt-key)
 
 (defmethod apply-mapping-opts :lookup-ref [{:keys [type func opt-key opt-value source target-property property-value]}]
-  (reset! *substitute* false)
+  (make-var 'do-mapping? false)
   (swap! source assoc target-property (opt-value property-value)))
 
 (defmethod apply-mapping-opts :ref-type [{:keys [type func opt-key opt-value source target-property property-value]}]
-  (reset! *substitute* false)
+  (make-var 'do-mapping? false)
   (swap! source assoc target-property (func property-value
                                             (var-by-symbol (-> opt-value
                                                                mapping-into-ns)))))
@@ -207,20 +215,20 @@
 
 (defn- map-property [mapping-func type from-property to-property source mapping-opts]
   (let [property-value (from-property source)]
-    (def ^:dynamic *substitute* (atom true))
-    (binding [*substitute* (atom true)]
-      (doseq [mapping-opt (keys mapping-opts)]
-        (apply-mapping-opts {:type type
-                             :func mapping-func
-                             :opt-key mapping-opt
-                             :opt-value (mapping-opt mapping-opts)
-                             :source source
-                             :target-property to-property
-                             :property-value property-value}))
-      (if (and (entity? property-value)
-               @*substitute*)
+    (make-var 'do-mapping? true)
+    (doseq [mapping-opt (keys mapping-opts)]
+      (apply-mapping-opts {:type type
+                           :func mapping-func
+                           :opt-key mapping-opt
+                           :opt-value (mapping-opt mapping-opts)
+                           :source source
+                           :target-property to-property
+                           :property-value property-value}))
+    (when (get-var 'do-mapping?)
+      (if (entity? property-value)
         (swap! source assoc to-property (mapping-func property-value))
-        (swap! source assoc to-property property-value))))
+        (swap! source assoc to-property property-value)))
+    (del-var 'do-mapping?))
   (swap! source dissoc from-property))
 
 (defn- has? [property mapping]
--- src/test/clj/core/db/entity_mappings_tests.clj
+++ src/test/clj/core/db/entity_mappings_tests.clj
@@ -20,47 +20,47 @@
                                     :in $ ?name
                                     :where [?p :db/ident ?name]] db prop-name))))))
 
-;; (deftest test-defentity-macro
-;;   (init {:mapping-inference true
-;;          :auto-persist-schema true
-;;          :db-url (mem-db-url)}
-;;        (defentity 'user.login
-;;             (property name :username  type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-;;             (property name :password  type :db.type/string                            mapping-opts {:required true})
-;;             (property name :roles     type :db.type/ref                               mapping-opts {:required true})
-;;             (property name :tenant    type :db.type/ref                               mapping-opts {:lookup-ref #([:user.login/username %])}))
-;;        (defentity 'tenant.login
-;;             (property name :username      type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-;;             (property name :password      type :db.type/string                            mapping-opts {:required true})
-;;             (property name :dburl         type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-;;             (property name :organization  type :db.type/string unique :db.unique/identity mapping-opts {:required true})))
-;;   (is (= :user.login/username (find-property-named :user.login/username (mem-db-url))))
-;;   (is (= :user.login/password (find-property-named :user.login/password (mem-db-url))))
-;;   (is (= :user.login/roles (find-property-named :user.login/roles (mem-db-url))))
-;;   (is (= :user.login/tenant (find-property-named :user.login/tenant (mem-db-url))))
-;;   (is (not (nil? (get-frequencies)))))
+(deftest test-defentity-macro
+  (defschema {:mapping-inference true
+              :auto-persist-schema true
+              :db-url (mem-db-url)}
+       (defentity 'user.login
+            (property name :username  type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+            (property name :password  type :db.type/string                            mapping-opts {:required true})
+            (property name :roles     type :db.type/ref                               mapping-opts {:required true})
+            (property name :tenant    type :db.type/ref                               mapping-opts {:lookup-ref #([:user.login/username %])}))
+       (defentity 'tenant.login
+            (property name :username      type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+            (property name :password      type :db.type/string                            mapping-opts {:required true})
+            (property name :dburl         type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+            (property name :organization  type :db.type/string unique :db.unique/identity mapping-opts {:required true})))
+  (is (= :user.login/username (find-property-named :user.login/username (mem-db-url))))
+  (is (= :user.login/password (find-property-named :user.login/password (mem-db-url))))
+  (is (= :user.login/roles (find-property-named :user.login/roles (mem-db-url))))
+  (is (= :user.login/tenant (find-property-named :user.login/tenant (mem-db-url))))
+  (is (not (nil? (get-frequencies)))))
 
-;; (deftest test-resolve-mapping
-;;   (init {:mapping-inference true
-;;          :auto-persist-schema true
-;;          :db-url (mem-db-url)}
-;;        (defentity 'user.login
-;;             (property name :username  type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-;;             (property name :password  type :db.type/string                            mapping-opts {:required true})
-;;             (property name :roles     type :db.type/ref                               mapping-opts {:required true})
-;;             (property name :tenant    type :db.type/ref                               mapping-opts {:lookup-ref #([:user.login/username %])}))
-;;        (defentity 'tenant.login
-;;             (property name :username      type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-;;             (property name :password      type :db.type/string                            mapping-opts {:required true})
-;;             (property name :dburl         type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-;;             (property name :organization  type :db.type/string unique :db.unique/identity mapping-opts {:required true})))
+(deftest test-resolve-mapping
+  (defschema {:mapping-inference true
+              :auto-persist-schema true
+              :db-url (mem-db-url)}
+       (defentity 'user.login
+            (property name :username  type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+            (property name :password  type :db.type/string                            mapping-opts {:required true})
+            (property name :roles     type :db.type/ref                               mapping-opts {:required true})
+            (property name :tenant    type :db.type/ref                               mapping-opts {:lookup-ref #([:user.login/username %])}))
+       (defentity 'tenant.login
+            (property name :username      type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+            (property name :password      type :db.type/string                            mapping-opts {:required true})
+            (property name :dburl         type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+            (property name :organization  type :db.type/string unique :db.unique/identity mapping-opts {:required true})))
 
-;;   (let [entity {:username "Wojtek"
-;;                 :password "sdakdshd"
-;;                 :dburl    "localhost:432"}]
-;;     (println (find-mapping entity))
-;;     (is (= 'tenant.login (-> (find-mapping entity)
-;;                              (:type))))))
+  (let [entity {:username "Wojtek"
+                :password "sdakdshd"
+                :dburl    "localhost:432"}]
+    (println (find-mapping entity))
+    (is (= 'tenant.login (-> (find-mapping entity)
+                             (:type))))))
 
 ;; (deftest test-cannot-resolve-mapping
 ;;  (init {:mapping-inference true
@@ -160,28 +160,27 @@
 ;;     (println (map-entity entity-vec))
 ;;     ))
 
-(deftest test-mapping-forth-and-back-1 []
-  (defschema {:mapping-inference true
-              :auto-persist-schema true
-              :db-url (mem-db-url)}
-       (defentity 'user.login
-            (property name :username  type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-            (property name :password  type :db.type/string                            mapping-opts {:required true})
-            (property name :roles     type :db.type/ref                               mapping-opts {:required true})
-            (property name :tenant    type :db.type/ref                               mapping-opts {:lookup-ref (fn [v] [:user.login/username v])}))
-       (defentity 'tenant.login
-            (property name :username      type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-            (property name :password      type :db.type/string                            mapping-opts {:required true})
-            (property name :dburl         type :db.type/string unique :db.unique/identity mapping-opts {:required true})
-            (property name :users         type :db.type/ref                               mapping-opts {:ref-type 'user.login})
-            (property name :organization  type :db.type/string unique :db.unique/identity mapping-opts {:required false})))
-    (let [entity-vec {:username "Wojtek"
-                      :password "adasd"
-                      :dburl    "localhost:432"}
-          db (clj->db entity-vec)
-          clj (db->clj db)]
-    (println "Mapping forth ========================================")
-    (println db)
-    (println "Mapping back  ========================================")
-    (println clj))
-    )
+;; (deftest test-mapping-forth-and-back-1 []
+;;   (defschema {:mapping-inference true
+;;               :auto-persist-schema true
+;;               :db-url (mem-db-url)}
+;;        (defentity 'user.login
+;;             (property name :username  type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+;;             (property name :password  type :db.type/string                            mapping-opts {:required true})
+;;             (property name :roles     type :db.type/ref                               mapping-opts {:required true})
+;;             (property name :tenant    type :db.type/ref                               mapping-opts {:lookup-ref (fn [v] [:user.login/username v])}))
+;;        (defentity 'tenant.login
+;;             (property name :username      type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+;;             (property name :password      type :db.type/string                            mapping-opts {:required true})
+;;             (property name :dburl         type :db.type/string unique :db.unique/identity mapping-opts {:required true})
+;;             (property name :users         type :db.type/ref                               mapping-opts {:ref-type 'user.login})
+;;             (property name :organization  type :db.type/string unique :db.unique/identity mapping-opts {:required false})))
+;;     (let [entity-vec {:username "Wojtek"
+;;                       :password "adasd"
+;;                       :dburl    "localhost:432"}
+;;           db (clj->db entity-vec)
+;;           clj (db->clj db)]
+;;     (println "Mapping forth ========================================")
+;;     (println db)
+;;     (println "Mapping back  ========================================")
+;;     (println clj)))

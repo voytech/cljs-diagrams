5062c720369478972a68a5a09a0467377f100bec
voytech <wojmak@gmail.com>
2015-09-29 23:33:02 +0200

    Testing creating tenant db functionality.[#103501076]

---
+++ src/test/clj/impl/services/tenant/manage_test.clj
@@ -0,0 +1,11 @@
+(ns impl.services.tenant.manage-test
+  (:require [core.services.tenant.manage :refer :all]
+            [app :refer [app-handler]]
+            [impl.services.test-helpers :refer :all]
+            [clojure.test :refer :all]
+            [tailrecursion.cljson  :as e :refer [cljson->clj clj->cljson]]
+            [ring.mock.request :as mock]
+            [impl.db.schema :refer :all]
+            [core.db.schemap :refer [persist-schema]]
+            [datomic.api :as d]
+            [core.services.base :refer :all]))

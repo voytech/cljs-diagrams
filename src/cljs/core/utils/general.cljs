547433bc82110e4a44566a7a7921196acb62580d
Wojciech Maka <wojciech.maka@inmarsat.com>
2017-07-25 10:42:15 +0200

    commit check

--- src/cljs/core/utils/general.cljs
+++ src/cljs/core/utils/general.cljs
@@ -6,10 +6,10 @@
 (defn make-js-property
   ([jsobj property value options]
    (.defineProperty js/Object jsobj property  (js-obj "value" value
-                                                       "writable"     (:writable options)
-                                                       "configurable" (:configurable options)
-                                                       "enumerable"   (:enumerable options))))
+                                                      "writable"     (:writable options)
+                                                      "configurable" (:configurable options)
+                                                      "enumerable"   (:enumerable options))))
   ([jsobj property value]
    (make-js-property jsobj property value {:writable true
-                                             :configurable true
-                                             :enumerable true})))
+                                           :configurable true
+                                           :enumerable true})))

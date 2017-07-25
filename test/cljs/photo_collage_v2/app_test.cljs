0da54c91ad42b19b0bf4236106fdca4330eea6c9
Wojciech Maka <wojciech.maka@inmarsat.com>
2017-07-25 20:35:44 +0200

    some initial cleanup

--- test/cljs/photo_collage_v2/app_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns photo-collage-v2.app-test
-  (:require-macros [cljs.test :refer [deftest testing is]])
-  (:require [cljs.test :as t]
-            [photo-collage-v2.app :as app]))
-
-(deftest test-arithmetic []
-  (is (= (+ 0.1 0.2) 0.3) "Something foul is a float."))

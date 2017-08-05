910a6bd484221de5ba2240760937f4c8d7f9fa5e
voytech <voytech.m@gmail.com>
2017-08-05 18:38:37 +0200

    implementing attributes and attribute values step3

--- src/cljs/impl/attributes.cljs
+++ src/cljs/impl/attributes.cljs
@@ -7,20 +7,22 @@
   {:cardinality 1
    :index 0
    :sync (fn [attr-value] (.setText (e/get-attribute-value-drawable-source attr-value "value") (:value attr-value)))}
+  {:width 100 :height 50} 
   [{:name "name"
     :type :title
     :src  (js/fabric.Text. "Name" options)}
    {:name "value"
     :type :value
-    :src (js/fabric.Text. (:value data) options)}])
+    :src (js/fabric.Text. (:value data) (merge options {:left (+ 50 (:left options))}))}])
 
 (defattribute description data options
   {:cardinality 1
    :index 1
    :sync (fn [attr-value] (.setText (e/get-attribute-value-drawable-source attr-value "value") (:value attr-value)))}
+  {:width 100 :height 50}
   [{:name "desc"
     :type :description
     :src  (js/fabric.Text. "Description" options)}
    {:name "value"
     :type :value
-    :src (js/fabric.Text. (:value data) options)}])
+    :src (js/fabric.Text. (:value data) (merge options {:left (+ 50 (:left options))}))}])

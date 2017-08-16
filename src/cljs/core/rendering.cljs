0890064e74c01221116b79aa36dc4dec11c94b69
voytech <voytech.m@gmail.com>
2017-08-16 16:15:46 +0200

    Implementing abstract rendering step 2.

--- src/cljs/core/rendering.cljs
+++ src/cljs/core/rendering.cljs
@@ -1,7 +1,11 @@
 (ns core.rendering
   (:require [core.eventbus :as bus]))
 
+(declare render)
+
 (def RENDERER (atom :fabric))
+
+(defonce rendering-context (atom {}))
 
 (defn set-rendering [renderer]
   (reset! RENDERER renderer))
@@ -9,7 +13,13 @@
 (defn get-rendering []
   @RENDERER)
 
-(defmulti do-render (fn [drawable] [@RENDERER (:type drawable)]))
+(bus/on ["drawable.changed"] -999 (fn [event])
+  (let [context (:context @event)
+        drawable (:drawable context)]
+     (swap! rendering-context assoc-in [(:property context)] (:new context))
+     (render drawable)))
+
+(defmulti do-render (fn [drawable context] [@RENDERER (:type drawable)]))
 
 (defmulti create-rendering-state (fn [type options] [@RENDERER type]))
 
@@ -17,4 +27,4 @@
   (let [rendering-state (:rendering-state drawable)]
     (when (nil? rendering-state)
       (update-state (create-rendering-state (:type drawable))))
-    (do-render drawable)))
+    (do-render drawable @rendering-context)))
--- src/cljs/impl/renderers/default.cljs
+++ src/cljs/impl/renderers/default.cljs
@@ -1,14 +1,29 @@
 (ns impl.renderers.default)
 
-(defmethod do-render [:fabric :rect] [drawable])
+(defn- set [property]
+  (fn [source value]
+    (.set source (clj->js {property value}))))
 
+(defn- properties [:left :top :width :height :x1 :x2 :y1 :y2 :border])
+
+(defonce property-mapping {:left (set :left)})
+
+(defn- property-change-render [drawable rendering-context]
+  (let [source (:rendering-state drawable)
+        prop   (:property rendering-context)
+        new    (:new rendering-context)]
+      ((prop property-mapping) source new)
+      (.setCoords source)))
+
+(defmethod do-render [:fabric :rect] [drawable rendering-context]
+  (property-change-render drawable rendering-context))
 
 (defmethod create-rendering-state [:fabric :rect] [drawable]
   (js/fabric.Rect. (clj->js (get-bbox drawable))))
 
 
-(defmethod do-render [:fabric :line] [drawable])
-
+(defmethod do-render [:fabric :line] [drawable rendering-context]
+  (property-change-render drawable rendering-context))
 
 (defmethod create-rendering-state [:fabric :line] [drawable]
   (js/fabric.Line. (clj->js [(get-left drawable)
@@ -17,7 +32,8 @@
                              (get-height drawable)])))
 
 
-(defmethod do-render [:fabric :triangle] [drawable])
+(defmethod do-render [:fabric :triangle] [drawable rendering-context]
+  (property-change-render drawable rendering-context))
 
 
 (defmethod create-rendering-state [:fabric :triangle] [drawable]

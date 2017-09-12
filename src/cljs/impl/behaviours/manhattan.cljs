341745805150b2f24a1d3942f9d5f8f097da3964
voytech <voytech.m@gmail.com>
2017-09-12 23:16:33 +0200

    Implementing manhattan layout for relation lines

--- src/cljs/impl/behaviours/manhattan.cljs
+++ src/cljs/impl/behaviours/manhattan.cljs
@@ -8,10 +8,13 @@
 
 ;; component assertion may be some abstraction in behabviours.
 
-(defn assert-component [entity name type]
-  (let [component (e/get-entity-component entity name)]
-    (when (or (nil? component)) (not= type (:type component))
-      (e/add-entity-component entity (e/new-component name type {})))))
+(defn assert-component
+  ([entity name type data]
+   (let [component (e/get-entity-component entity name)]
+     (when (or (nil? component)) (not= type (:type component))
+       (e/add-entity-component entity (e/new-component type name data {})))))
+  ([entity name type]
+   (assert-component entity name type {})))
 
 (defn assert-components [entity])
 
@@ -37,26 +40,42 @@
     [[start-point (first mid-points)] mid-points [(last mid-points end-point)]]
     [[start-point (first mid-points)] [(last mid-points end-point)]]))
 
-(defn- update-line-components [entity path])
+(defn- update-line-component [entity idx sx sy ex ey]
+  (let [line (assert-component entity (str "line-" idx) :relation {:x1 sx :y1 sy :x2 ex :y2 ey})]))
+
+
+(defn- update-line-components [entity path]
+  (map (fn [idx item] (update-line-components entity idx (first (first item))
+                                                         (peek (first item))
+                                                         (first (second item))
+                                                         (peek (second item)))) path))
 
 (defn initialise-manhattan-layout [entity s-normal e-normal]
   (let [start (e/get-entity-component entity "start")
         end (e/get-entity-component entity "end")
         connector (e/get-entity-component entity "connector")
         mid-points (compute-mid-points start end s-normal e-normal)
-        path (compute-path (center-point start) (center-point end) mid-points)]))
-
+        path (compute-path (center-point start) (center-point end) mid-points)]
+     (update-line-components entity path)))
 
 (defn update-manhattan-layout [entity s-normal e-normal]
   (let [start (e/get-entity-component entity "start")
         end (e/get-entity-component entity "end")
         connector (e/get-entity-component entity "connector")]))
 
-(defn manhattan-layout-moving []
+(defn calculate-normals [entity startpoint endpoint]
+  [:h :h])
+
+(defn manhattan-layout-moving-behaviour []
   (fn [e]
      (let [endpoint (:component e)
            entity   (:entity e)
-           drawable (:drawable endpoint)]
+           drawable (:drawable endpoint)
+           start (e/get-entity-component entity "start")
+           end (e/get-entity-component entity "end")
+           connector (e/get-entity-component entity "connector")
+           normals (calculate-normals entity start end)]
+        (initialise-manhattan-layout entity (first normals) (last normals))   
         (cond
           (= :startpoint (:type endpoint)) (position-startpoint entity (:movement-x e) (:movement-y e) :offset)
           (= :endpoint   (:type endpoint)) (position-endpoint   entity (:movement-x e) (:movement-y e) :offset))))  `)

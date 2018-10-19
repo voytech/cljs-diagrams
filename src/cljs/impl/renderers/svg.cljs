db30388351ba46ec135e8165a39f68b79e5a657e
Mąka Wojciech (PWC) <wojciech.maka@euro.com.pl>
2018-10-19 16:18:17 +0200

    Starting implementing svg renderer

---
+++ src/cljs/impl/renderers/svg.cljs
@@ -0,0 +1,7 @@
+(ns impl.renderers.svg
+  (:require [core.utils.general :refer [make-js-property]]
+            [core.components :as d]
+            [core.entities :as e]
+            [core.eventbus :as b]
+            [core.rendering :as r]
+            [impl.components :as impld]))
